6d79d9b29ef20ba740c83e16b8161fc7dd8e3234
Li Ming <limingth@gmail.com>
2012-07-25 16:19:40 +0800

    Update ARM9-S3C24x0-20120716/bootloader.c

--- ARM9-S3C24x0-20120716/bootloader.c
+++ ARM9-S3C24x0-20120716/bootloader.c
@@ -1,44 +1,68 @@
 
-void nand_read_page(int nand_addr, char * buf)
+#ifdef S3C2440
+
+#define NFCONF		(*(volatile unsigned int *)(0x4E000000))
+#define NFCONT		(*(volatile unsigned int *)(0x4E000004))
+#define NFCMMD		(*(volatile unsigned char *)(0x4E000008))
+#define NFADDR		(*(volatile unsigned char *)(0x4E00000C))
+#define NFDATA		(*(volatile unsigned char *)(0x4E000010))
+#define NFSTAT		(*(volatile unsigned char *)(0x4E000020))
+
+void nand_init(void)
 {
-	int i = 0;
-
-	// Nand Flash Read
-	//	1. D¡ä NFCMMD = 0x00
-	NFCMD = 0x00;
-
-	//	2. D¡ä 4 ¡ä? NFADDR
-	NFADDR = nand_addr & 0xff;		// A0-A7
-	NFADDR = (nand_addr >> 9) & 0xff;	// A9-A16
-	NFADDR = (nand_addr >> 17) & 0xff;	// A17-A24
-	NFADDR = (nand_addr >> 25) & 0xff;	// A25
-
-	//	3. ???¡¥ NFSTAT¡ê??¡ä R/nB
-	while ((NFSTAT & (1<<0)) == 0)
-		;
-
-	//	4. ?¨¢ 512 ¡ä? NFDATA
-	for (i = 0; i < 512; i++)
-	{
-		*buf++ = NFDATA & 0xFF;
-	}
-}
-
-void nand_read(int nand_addr, int sdram_addr, int size)
-{	
-	int pages;
-	int i;
-	char * buf = (char *)sdram_addr;
+	// TACLS [13:12]
+	// TWRPH0 [10:8]
+	// TWRPH1 [6:4]
+	// AdvFlash (Read only) [3] 0: Support   256 or   512 byte/page NAND flash memory
+	// PageSize (Read only) [2]	1: 512 Bytes/page
+	// AddrCycle (Read only) [1]	1: 4 address cycle
+	// BusWidth (R/W) [0]		0: 8-bit bus
+	NFCONF = 3<<12 | 7<<8 | 7<<4;
 	
-	if (size <= 0)
-		return;
-
-	// get how many pages to be read
-	pages = (size - 1) / 512 + 1;
-
-	for (i = 0; i < pages; i++)
-		nand_read_page(nand_addr+i*512, buf+i*512);
+	// Reg_nCE [1]		0: Force nFCE to low (Enable chip select)
+	// MODE [0]		1: NAND flash controller enable	
+	NFCONT = 1<<0 | 0<<1;
+	
+	return;
 }
+
+#endif
+
+#ifdef S3C2410
+
+#define NFCONF	(*(volatile unsigned int *)0x4e000000)
+//#define NFCONT	(*(volatile unsigned int *)0x4e000004)
+#define NFCMMD	(*(volatile unsigned char *)0x4e000004)
+#define NFADDR	(*(volatile unsigned char *)0x4e000008)
+#define NFDATA	(*(volatile unsigned char *)0x4e00000c)
+#define NFSTAT	(*(volatile unsigned char *)0x4e000010)
+
+void nand_init(void)
+{
+	//NFCONF = 0x1400;
+	//NFCONT = 0x01;
+	
+	// TWRPH0 [6:4] TWRPH0 duration setting value (0~7)
+	// Duration = HCLK * (TWRPH0 + 1)
+	NFCONF |= 1<<6;
+	
+	// Enable/Disable [15] NAND Flash controller enable/disable
+	// 0 = Disable NAND Flash Controller
+	// 1 = Enable NAND Flash Controller
+	NFCONF |= 1<<15;
+	
+	// TACLS [10:8] CLE & ALE duration setting value (0~7)
+	// Duration = HCLK * (TACLS + 1)
+	//NFCONF |= 1<<8;
+	
+	// NAND Flash Memory	chip enable
+  	//	[11]   NAND flash memory nFCE control
+  	//	0 : NAND flash nFCE = L (active)
+	//	1 : NAND flash nFCE = H (inactive)
+  	//	(After auto-boot, nFCE will be inactive.)	
+  	NFCONF &= ~(1<<11);
+}
+#endif
 
 /*
  * in this stdio.c, '\n' will be treated as '\r'+'\n'
