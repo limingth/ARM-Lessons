3f2315d5ecf797d9ed4cb4ac505ff7c93561b203
Li Ming <limingth@gmail.com>
2012-08-24 17:00:29 +0800

    add dm9000 driver

---
+++ CortexA8-s5pv210-20120901/tiny210/hands-on/8-dm9000/.dm9000.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/limingth/tiny210/hands-on/8-dm9000/dm9000.ko := arm-linux-ld -EL -r  -T /home/limingth/tiny210/src/linux-2.6.35.7/scripts/module-common.lds --build-id -o /home/limingth/tiny210/hands-on/8-dm9000/dm9000.ko /home/limingth/tiny210/hands-on/8-dm9000/dm9000.o /home/limingth/tiny210/hands-on/8-dm9000/dm9000.mod.o
---
+++ CortexA8-s5pv210-20120901/tiny210/hands-on/8-dm9000/.tmp_versions/dm9000.mod
@@ -0,0 +1,2 @@
+/home/limingth/tiny210/hands-on/8-dm9000/dm9000.ko
+/home/limingth/tiny210/hands-on/8-dm9000/dm9000.o
